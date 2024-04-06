613b173d8770ec4facd445f853328ddb82f320f3
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-04-07 04:40:17 +0500

    Add files via upload

---
+++ ans6.js
@@ -0,0 +1,8 @@
+var Addition = 5 + 3;
+console.log(Addition);
+var Substraction = 10 - 2;
+console.log(Substraction);
+var Multiplication = 2 * 4;
+console.log(Multiplication);
+var Division = 16 / 2;
+console.log(Division);
