f02169a716f7faa47e801bcdac7211fe1f575419
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-04-23 02:22:42 +0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,5 @@
+var names = ["faiza", "shiza", "shagufta", "maliha"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hello, ".concat(name_1, " \"Would you like to drink coffee?\""));
+}
