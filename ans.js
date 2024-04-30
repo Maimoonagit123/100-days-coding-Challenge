53183f4e224ef01f647948f56c9748ae191137e6
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-05-01 01:37:06 +0500

    Add files via upload

---
+++ ans.js
@@ -0,0 +1,4 @@
+var transports = ["Tesla car", "Toyota Fortuner", "Honda Motorbike"];
+transports.forEach(function (transport) {
+    console.log("I would like to own a ".concat(transport, "."));
+});
