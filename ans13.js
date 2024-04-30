80bff56f4eff5203089684d02305237d3e72d499
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-05-01 02:07:42 +0500

    Add files via upload

---
+++ ans13.js
@@ -0,0 +1,11 @@
+var myFriendslist = [
+    "Urwah",
+    "Ayan",
+    "Zurain",
+    "Sanan",
+    "Faraz",
+    "Hussain",
+];
+myFriendslist.forEach(function (friend) {
+    console.log("Hello ".concat(friend, ", We're excited to extend an invitation to a dinner party at our place. It's been far too long since we've had a chance to catch up and enjoy some good food together."));
+});
