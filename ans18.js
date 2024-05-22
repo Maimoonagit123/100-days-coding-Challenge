4f4a01dbfa006b15a3308a14b6c5d47c03ae0874
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-05-22 23:15:42 +0500

    Add files via upload

---
+++ ans18.js
@@ -0,0 +1,4 @@
+var dinnerguests = ["Maimoona", "Shoaib", "Sahana", "Zulfiqar", "Shams", "Subhan Al Lazi", "Zaroon"];
+dinnerguests.forEach(function (guest) {
+    console.log("\"Dear! ".concat(guest, " I would like to invite you at my wedding..."));
+});
