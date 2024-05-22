04e5ce9ea1e0b28c51fef4b1f6ac7b1325732c78
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-05-23 00:20:39 +0500

    Add files via upload

---
+++ ans20.js
@@ -0,0 +1,6 @@
+var bookInfo = {
+    title: "Impact of technology in the classroom",
+    author: "Maimoona Masood",
+    publishYear: 2023
+};
+console.log("Book info: ".concat(bookInfo.title, ",by ").concat(bookInfo.author, ", published in ").concat(bookInfo.publishYear, "...\n"));
