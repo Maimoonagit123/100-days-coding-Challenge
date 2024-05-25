8f0365ec6d67b9d7a212905a1eaeb856b9d86395
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-05-26 03:56:52 +0500

    Add files via upload
    😍uploaded👏

---
+++ ans24.js
@@ -0,0 +1,14 @@
+var fruit = ["Mango", "Apple"];
+//its about string condition
+console.log("Mango" == "Mango");
+var car = "Ferrari";
+console.log("Ferrari".toLowerCase() == "ferrari");
+var numeric = 8;
+console.log(5 > 10);
+console.log("Tests with 'and' and 'or':");
+console.log(true && false); // False
+var fruits = ["apple", "banana", "cherry"];
+console.log("Is 'apple' in fruits?");
+console.log(fruits.includes("apple")); // True
+console.log("Is 'mango' not in fruits?");
+console.log(!fruits.includes("mango"));
