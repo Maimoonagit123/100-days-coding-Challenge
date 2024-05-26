2ed81ae714cdb03267f8ee160efe0f5f3e5217ac
Maimoona Masood Ferozi <132249128+Maimoonagit123@users.noreply.github.com>
2024-05-26 05:32:45 +0500

    Add files via upload

---
+++ ans29.js
@@ -0,0 +1,10 @@
+var favorite_fruits = ["Apples", "Bananas", "Cherries", "Strawberries"];
+if (favorite_fruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+else if (favorite_fruits.includes("Strawberries")) {
+    console.log("You really like strawberries");
+}
